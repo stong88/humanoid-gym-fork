a5992d3875e9c9acab51842f5188ff430c7d7ac7
Bruno-Dumont <brunodemoraesdumont@gmail.com>
2026-03-03 15:22:18 -0800

    Changed num bins for experiment

--- humanoid/scripts/train.py
+++ humanoid/scripts/train.py
@@ -38,7 +38,7 @@ def train(args):
     env, env_cfg = task_registry.make_env(name=args.task, args=args)
 
     # uses the discrete action wrapper 
-    env = DiscreteActionsWrapper(env, num_bins=7)
+    env = DiscreteActionsWrapper(env, num_bins=15)
     ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args)
     ppo_runner.learn(num_learning_iterations=train_cfg.runner.max_iterations, init_at_random_ep_len=True)
 
