47bf7338b3a6cd86775d86918dc7fa076ad5d577
Bruno-Dumont <brunodemoraesdumont@gmail.com>
2026-03-16 15:25:53 -0700

    Added the grpo

---
+++ humanoid/algo/ppo/grpo_original.py
@@ -0,0 +1,148 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021 ETH Zurich, Nikita Rudin
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2024 Beijing RobotEra TECHNOLOGY CO.,LTD. All rights reserved.
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from .ppo import PPO
+from .actor_critic import ActorCritic
+from .rollout_storage import RolloutStorage
+
+class GRPOOriginal(PPO):
+    actor_critic: ActorCritic
+    def __init__(self,
+                 actor_critic,
+                 num_envs_per_group=8,
+                 num_learning_epochs=1,
+                 num_mini_batches=1,
+                 clip_param=0.2,
+                 gamma=0.998,
+                 lam=0.95,
+                 entropy_coef=0.0,
+                 learning_rate=1e-3,
+                 max_grad_norm=1.0,
+                 schedule="fixed",
+                 desired_kl=0.01,
+                 device='cpu',
+                 ):
+
+        self.device = device
+
+        self.num_envs_per_group = num_envs_per_group
+
+        self.desired_kl = desired_kl
+        self.schedule = schedule
+        self.learning_rate = learning_rate
+
+        # PPO components
+        self.actor_critic = actor_critic
+        self.actor_critic.to(self.device)
+        self.storage = None # initialized later
+        self.optimizer = optim.Adam(self.actor_critic.parameters(), lr=learning_rate)
+        self.transition = RolloutStorage.Transition()
+
+        # PPO parameters
+        self.clip_param = clip_param
+        self.num_learning_epochs = num_learning_epochs
+        self.num_mini_batches = num_mini_batches
+        self.entropy_coef = entropy_coef
+        self.gamma = gamma
+        self.lam = lam
+        self.max_grad_norm = max_grad_norm
+
+        print(f'GRPOOriginal: clipping parameter {self.clip_param}')
+
+    def compute_returns(self, last_critic_obs):
+        returns = torch.zeros_like(self.storage.rewards)
+        intermediate_returns = torch.zeros_like(self.storage.rewards[0])
+        
+        for step in reversed(range(self.storage.num_transitions_per_env)):
+            non_terminal = 1.0 - self.storage.dones[step].float()
+            intermediate_returns = self.storage.rewards[step] + self.gamma * intermediate_returns * non_terminal
+            returns[step] = intermediate_returns
+
+        self.storage.returns = returns
+        
+        
+        num_steps_per_env, num_envs, _ = returns.shape
+        
+        assert num_envs % self.num_envs_per_group == 0, "Total envs should be evenly divisible by num_envs_per_group"
+        num_groups = num_envs // self.num_envs_per_group
+        
+        returns_grouped = returns.reshape(num_steps_per_env, num_groups, self.num_envs_per_group, 1)
+        
+        group_mean = returns_grouped.mean(dim=2, keepdim=True)
+        group_std = returns_grouped.std(dim=2, keepdim=True)
+        
+        advantages_grouped = (returns_grouped - group_mean) / (group_std + 1e-6)
+        
+        self.storage.advantages = advantages_grouped.reshape(num_steps_per_env, num_envs, 1)
+
+    def update(self):
+        # Identical to ppo update, but remove value computation
+        mean_surrogate_loss = 0
+
+        generator = self.storage.mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+        for obs_batch, critic_obs_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, old_actions_log_prob_batch, \
+            old_mu_batch, old_sigma_batch, hid_states_batch, masks_batch in generator:
+
+
+                self.actor_critic.act(obs_batch, masks=masks_batch, hidden_states=hid_states_batch[0])
+                actions_log_prob_batch = self.actor_critic.get_actions_log_prob(actions_batch)
+                value_batch = self.actor_critic.evaluate(critic_obs_batch, masks=masks_batch, hidden_states=hid_states_batch[1])
+                mu_batch = self.actor_critic.action_mean
+                sigma_batch = self.actor_critic.action_std
+                entropy_batch = self.actor_critic.entropy
+
+                # Surrogate loss
+                ratio = torch.exp(actions_log_prob_batch - torch.squeeze(old_actions_log_prob_batch))
+                surrogate = -torch.squeeze(advantages_batch) * ratio
+                surrogate_clipped = -torch.squeeze(advantages_batch) * torch.clamp(ratio, 1.0 - self.clip_param,
+                                                                                1.0 + self.clip_param)
+                surrogate_loss = torch.max(surrogate, surrogate_clipped).mean()
+
+
+                loss = surrogate_loss - self.entropy_coef * entropy_batch.mean()
+
+                # Gradient step
+                self.optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+
+                mean_surrogate_loss += surrogate_loss.item()
+
+        num_updates = self.num_learning_epochs * self.num_mini_batches
+        mean_surrogate_loss /= num_updates
+        self.storage.clear()
+
+        return 0, mean_surrogate_loss
