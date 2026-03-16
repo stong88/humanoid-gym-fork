fix: fix policy_indices computation

humanoid/algo/ppo/rollout_storage.py
@@ -241,7 +241,8 @@ class RolloutStorage:
         else:
             critic_observations = observations
 
-        policy_indices = torch.arange(num_policies).repeat_interleave(self.num_envs * self.num_transitions_per_env).to(device=self.device)  # (num_envs * num_transitions_per_env)
+        # (num_envs * num_transitions_per_env)
+        policy_indices = torch.arange(num_policies).repeat_interleave((self.num_envs // num_policies) * self.num_transitions_per_env).to(device=self.device)
         assert policy_indices.shape[0] == observations.shape[0]
 
         actions = self.actions.flatten(0, 1)